4602184516cf48c7ba4c43df8a06124115763be7
RalphsCode <124472641+RalphsCode@users.noreply.github.com>
2023-05-18 12:33:05 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+// Define your routes and other functionality
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
